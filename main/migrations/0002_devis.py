d2cde3bd8bea9df6bae8acee9e5a631d4b5c9e0f
Snow275 <kasse.coura27@gmail.com>
2026-03-01 13:53:13 -0500

    Update 0002_devis.py

--- main/migrations/0002_devis.py
+++ main/migrations/0002_devis.py
@@ -1,5 +1,4 @@
 from django.db import migrations, models
-import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
@@ -23,7 +22,6 @@ class Migration(migrations.Migration):
                 ('notes', models.TextField(blank=True, default='')),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
                 ('updated_at', models.DateTimeField(auto_now=True)),
-                ('document', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='devis', to='main.document')),
             ],
             options={
                 'verbose_name': 'Devis',
