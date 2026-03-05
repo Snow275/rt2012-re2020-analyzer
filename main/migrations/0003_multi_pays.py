679c3575db6031c4c0d5ff2fdd97e6ded737283e
Snow275 <kasse.coura27@gmail.com>
2026-03-05 14:11:33 -0500

    Add files via upload

---
+++ main/migrations/0003_multi_pays.py
@@ -0,0 +1,64 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('main', '0002_devis_document'),
+    ]
+
+    operations = [
+        migrations.RunSQL(
+            sql="""
+                ALTER TABLE main_document
+                    ADD COLUMN IF NOT EXISTS pays VARCHAR(5) NOT NULL DEFAULT 'FR',
+                    ADD COLUMN IF NOT EXISTS norme VARCHAR(10) NOT NULL DEFAULT 'RE2020',
+
+                    ADD COLUMN IF NOT EXISTS peb_espec DOUBLE PRECISION NULL,
+                    ADD COLUMN IF NOT EXISTS peb_ew DOUBLE PRECISION NULL,
+                    ADD COLUMN IF NOT EXISTS peb_u_mur DOUBLE PRECISION NULL,
+                    ADD COLUMN IF NOT EXISTS peb_u_toit DOUBLE PRECISION NULL,
+                    ADD COLUMN IF NOT EXISTS peb_u_plancher DOUBLE PRECISION NULL,
+
+                    ADD COLUMN IF NOT EXISTS minergie_qh DOUBLE PRECISION NULL,
+                    ADD COLUMN IF NOT EXISTS minergie_qtot DOUBLE PRECISION NULL,
+                    ADD COLUMN IF NOT EXISTS minergie_n50 DOUBLE PRECISION NULL,
+
+                    ADD COLUMN IF NOT EXISTS sia380_qh DOUBLE PRECISION NULL,
+
+                    ADD COLUMN IF NOT EXISTS cneb_ei DOUBLE PRECISION NULL,
+                    ADD COLUMN IF NOT EXISTS cneb_u_mur DOUBLE PRECISION NULL,
+                    ADD COLUMN IF NOT EXISTS cneb_u_toit DOUBLE PRECISION NULL,
+                    ADD COLUMN IF NOT EXISTS cneb_u_fenetre DOUBLE PRECISION NULL,
+                    ADD COLUMN IF NOT EXISTS cneb_infiltration DOUBLE PRECISION NULL,
+
+                    ADD COLUMN IF NOT EXISTS lenoz_ep DOUBLE PRECISION NULL,
+                    ADD COLUMN IF NOT EXISTS lenoz_ew DOUBLE PRECISION NULL,
+                    ADD COLUMN IF NOT EXISTS lenoz_u_mur DOUBLE PRECISION NULL,
+                    ADD COLUMN IF NOT EXISTS lenoz_u_toit DOUBLE PRECISION NULL;
+            """,
+            reverse_sql="""
+                ALTER TABLE main_document
+                    DROP COLUMN IF EXISTS pays,
+                    DROP COLUMN IF EXISTS norme,
+                    DROP COLUMN IF EXISTS peb_espec,
+                    DROP COLUMN IF EXISTS peb_ew,
+                    DROP COLUMN IF EXISTS peb_u_mur,
+                    DROP COLUMN IF EXISTS peb_u_toit,
+                    DROP COLUMN IF EXISTS peb_u_plancher,
+                    DROP COLUMN IF EXISTS minergie_qh,
+                    DROP COLUMN IF EXISTS minergie_qtot,
+                    DROP COLUMN IF EXISTS minergie_n50,
+                    DROP COLUMN IF EXISTS sia380_qh,
+                    DROP COLUMN IF EXISTS cneb_ei,
+                    DROP COLUMN IF EXISTS cneb_u_mur,
+                    DROP COLUMN IF EXISTS cneb_u_toit,
+                    DROP COLUMN IF EXISTS cneb_u_fenetre,
+                    DROP COLUMN IF EXISTS cneb_infiltration,
+                    DROP COLUMN IF EXISTS lenoz_ep,
+                    DROP COLUMN IF EXISTS lenoz_ew,
+                    DROP COLUMN IF EXISTS lenoz_u_mur,
+                    DROP COLUMN IF EXISTS lenoz_u_toit;
+            """,
+        ),
+    ]
