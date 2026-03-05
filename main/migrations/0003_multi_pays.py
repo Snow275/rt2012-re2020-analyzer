7ac65ab51a37d11b16fb98716e6b49d9827f2c7a
Snow275 <kasse.coura27@gmail.com>
2026-03-05 14:14:12 -0500

    Update 0003_multi_pays.py

--- main/migrations/0003_multi_pays.py
+++ main/migrations/0003_multi_pays.py
@@ -3,35 +3,35 @@ from django.db import migrations
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ('main', '0002_devis_document'),
-    ]
+    dependencies = []
 
     operations = [
         migrations.RunSQL(
             sql="""
                 ALTER TABLE main_document
+                    ADD COLUMN IF NOT EXISTS rapport_pdf VARCHAR(100) NULL;
+
+                ALTER TABLE main_devis
+                    ADD COLUMN IF NOT EXISTS document_id bigint NULL
+                    REFERENCES main_document(id) ON DELETE SET NULL;
+
+                ALTER TABLE main_document
                     ADD COLUMN IF NOT EXISTS pays VARCHAR(5) NOT NULL DEFAULT 'FR',
                     ADD COLUMN IF NOT EXISTS norme VARCHAR(10) NOT NULL DEFAULT 'RE2020',
-
                     ADD COLUMN IF NOT EXISTS peb_espec DOUBLE PRECISION NULL,
                     ADD COLUMN IF NOT EXISTS peb_ew DOUBLE PRECISION NULL,
                     ADD COLUMN IF NOT EXISTS peb_u_mur DOUBLE PRECISION NULL,
                     ADD COLUMN IF NOT EXISTS peb_u_toit DOUBLE PRECISION NULL,
                     ADD COLUMN IF NOT EXISTS peb_u_plancher DOUBLE PRECISION NULL,
-
                     ADD COLUMN IF NOT EXISTS minergie_qh DOUBLE PRECISION NULL,
                     ADD COLUMN IF NOT EXISTS minergie_qtot DOUBLE PRECISION NULL,
                     ADD COLUMN IF NOT EXISTS minergie_n50 DOUBLE PRECISION NULL,
-
                     ADD COLUMN IF NOT EXISTS sia380_qh DOUBLE PRECISION NULL,
-
                     ADD COLUMN IF NOT EXISTS cneb_ei DOUBLE PRECISION NULL,
                     ADD COLUMN IF NOT EXISTS cneb_u_mur DOUBLE PRECISION NULL,
                     ADD COLUMN IF NOT EXISTS cneb_u_toit DOUBLE PRECISION NULL,
                     ADD COLUMN IF NOT EXISTS cneb_u_fenetre DOUBLE PRECISION NULL,
                     ADD COLUMN IF NOT EXISTS cneb_infiltration DOUBLE PRECISION NULL,
-
                     ADD COLUMN IF NOT EXISTS lenoz_ep DOUBLE PRECISION NULL,
                     ADD COLUMN IF NOT EXISTS lenoz_ew DOUBLE PRECISION NULL,
                     ADD COLUMN IF NOT EXISTS lenoz_u_mur DOUBLE PRECISION NULL,
@@ -39,6 +39,7 @@ class Migration(migrations.Migration):
             """,
             reverse_sql="""
                 ALTER TABLE main_document
+                    DROP COLUMN IF EXISTS rapport_pdf,
                     DROP COLUMN IF EXISTS pays,
                     DROP COLUMN IF EXISTS norme,
                     DROP COLUMN IF EXISTS peb_espec,
@@ -59,6 +60,9 @@ class Migration(migrations.Migration):
                     DROP COLUMN IF EXISTS lenoz_ew,
                     DROP COLUMN IF EXISTS lenoz_u_mur,
                     DROP COLUMN IF EXISTS lenoz_u_toit;
+
+                ALTER TABLE main_devis
+                    DROP COLUMN IF EXISTS document_id;
             """,
         ),
     ]
