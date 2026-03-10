2fa881276353a441aec388829e0d8f67070903b0
Snow275 <kasse.coura27@gmail.com>
2026-03-10 15:39:43 -0400

    Update 0005_document_date_debut_analyse.py

--- main/migrations/0005_document_date_debut_analyse.py
+++ main/migrations/0005_document_date_debut_analyse.py
@@ -1,16 +1,15 @@
-from django.db import migrations, models
+from django.db import migrations
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('main', '0003_multi_pays'),  
+        ('main', '0003_multi_pays'),
     ]
 
     operations = [
-        migrations.AddField(
-            model_name='document',
-            name='date_debut_analyse',
-            field=models.DateField(blank=True, null=True),
+        migrations.RunSQL(
+            sql="ALTER TABLE main_document ADD COLUMN IF NOT EXISTS date_debut_analyse DATE NULL;",
+            reverse_sql="ALTER TABLE main_document DROP COLUMN IF EXISTS date_debut_analyse;",
         ),
     ]
